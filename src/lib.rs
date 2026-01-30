8fd22082392dee50f1a0a800fc52e0fa4f6ef590
Paulo Vítor Sarmento <vsarmento@proton.me>
2026-01-30 11:18:23 -0300

    feat: Add `minijinja` with autoreload

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
-use axum::{Router, http::StatusCode, routing};
+use std::sync::Arc;
+
+use axum::{
+    Router,
+    extract::State,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing,
+};
+use minijinja::{Environment, context, path_loader};
+use minijinja_autoreload::AutoReloader;
 
 // Root handler.
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn root(State(state): State<AppState>) -> impl IntoResponse {
+    Html(state.render_template("base.html", context! {}))
 }
 
 // Return `200 OK` if the server is running.
@@ -11,7 +21,33 @@ async fn health_check() -> StatusCode {
 }
 
 pub fn app() -> Router {
+    let reloader = AutoReloader::new(|notifier| {
+        let template_path = "templates";
+        let mut env = Environment::new();
+        env.set_loader(path_loader(template_path));
+        notifier.watch_path(template_path, true);
+        Ok(env)
+    });
+
+    let app_state = AppState {
+        jinja_env: Arc::new(reloader),
+    };
+
     Router::new()
         .route("/", routing::get(root))
         .route("/health_check", routing::get(health_check))
+        .with_state(app_state)
+}
+
+#[derive(Clone)]
+struct AppState {
+    jinja_env: Arc<AutoReloader>,
+}
+
+impl AppState {
+    fn render_template(&self, name: &str, ctx: minijinja::Value) -> String {
+        let env = self.jinja_env.acquire_env().unwrap();
+        let template = env.get_template(name).unwrap();
+        template.render(ctx).unwrap()
+    }
 }
